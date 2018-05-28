25b8e763051a1df5935f0148a38b7fc6ba7c8c40
DucauArley <36281515+DucauArley@users.noreply.github.com>
2018-05-28 20:42:59 -0300

    Add files via upload

---
+++ memoriaDinamica/main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int* vector;
+    int i;
+    int* aux;
+
+    vector = (int*) malloc(sizeof(int) * 5);
+
+    if(vector == NULL)
+    {
+        printf("No se pudo adquirir memoria\n");
+        exit(1);
+    }
+
+    for(i=0;i<5;i++)
+    {
+        printf("Ingrese un numero\n");
+        scanf("%d", vector + i);
+    }
+
+
+    for(i=0;i<5;i++)
+    {
+        printf("%d\n", *(vector + i));
+    }
+    printf("\n\n");
+
+    aux = (int*) realloc(vector, 10 * sizeof(int));
+
+    if(aux != NULL)
+    {
+        vector = aux;
+        printf("Se agrando el array con exito\n");
+    }
+    else
+    {
+        printf("No se puede agrandar el array\n");
+    }
+
+     for(i=5;i<10;i++)
+    {
+        printf("Ingrese un numero\n");
+        scanf("%d", vector + i);
+    }
+
+
+    for(i=0;i<10;i++)
+    {
+        printf("%d\n", *(vector + i));
+    }
+    printf("\n\n");
+
+    printf("se achico el array a 6 elementos");
+
+    vector = (int*) realloc(vector, 6 * sizeof(int));
+
+    for(i=0;i<10;i++)
+    {
+        printf("%d\n", *(vector + i));
+    }
+    printf("\n\n");
+
+
+    return 0;
+}
